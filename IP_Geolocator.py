0859e89cc9f0b3584232bb354d9523e834d8f578
deekb <87583003+deekb@users.noreply.github.com>
2022-07-22 21:07:02 -0400

    Update IP_Geolocator.py
    Ensured Linux & Windows compatibility
    Now using Rich API to style output
    Removed unused imports
    Checked against PEP8 Styling
    Renamed variables and constants to reflect their purpose

--- IP_Geolocator.py
+++ IP_Geolocator.py
@@ -1,78 +1,92 @@
 #!/bin/python3
 import json
-import socket
+from rich.console import Console
+from rich import print
 from urllib.request import urlopen
-URL = "http://ip-api.com/json/"
+GEOLOCATION_API = "http://ip-api.com/json/"
+
+
+def getIP():
+    return(urlopen("https://api.ipify.org/").read().decode())
+
 
 def helpMenu():
-    return("Usage: [OPTIONS]... [IP Address(V4/V6)]...\n" + \
-           "Options:\n" + \
-           "\tAll\t\tPrint all data about the address\n" + \
-           "\tCity\t\tPrint the city of the address\n" + \
-           "\tZIP\t\tPrint the ZIP code of the address\n" + \
-           "\tISP\t\tPrint the Internet Service Provider(ISP) of the address\n" + \
-           "\tCountry\t\tPrint the country of an address\n" + \
-           "\tRegion\t\tPrint the region of an address\n" + \
-           "\tTime Zone\tPrint the time zone of an address\n" + \
-           "IP Address(V4/V6):\n" + \
-           "\t\"~\"\t\tDenotes your global ip address")
+    return("Usage: [OPTIONS]... [IP Address(V4/V6)]\n" +
+           "Options:\n" +
+           "\tAll\t\tPrint all data about the address\n" +
+           "\tCity\t\tPrint the city of the address\n" +
+           "\tZIP\t\tPrint the ZIP code of the address\n" +
+           "\tISP\t\tPrint the Internet Service Provider(ISP) of the address\n" +
+           "\tCountry\t\tPrint the country of an address\n" +
+           "\tRegion\t\tPrint the region of an address\n" +
+           "\tTime Zone\tPrint the time zone of an address\n" +
+           "IP Address(V4/V6):\n")
+
 
 def app():
     while True:
-        ip = input("Command (\"?\" for help): ").lower()
-        if ip == "?":
-            print(helpMenu())
+        query = input("Command (\"?\" for help): ").lower()
+        if query == "?":
+            console = Console(highlight = False)
+            console.print(helpMenu())
             continue
-        elif "~" in ip:
-            ip = ip.replace("~", urlopen("https://api.ipify.org/").read().decode())
-        
-        shownFields = []
-        if "all" not in ip:
-            if "city" in ip:
-                shownFields.append(["\tCity: ", "city"])
-                ip = ip.replace("city", "")
-            if "zip" in ip:
-                shownFields.append(["\tZIP code: ", "zip"])
-                ip = ip.replace("zip", "")
-            if "isp" in ip:
-                shownFields.append(["\tISP: ", "isp"])
-                ip = ip.replace("isp", "")
-            if "country" in ip:
-                shownFields.append(["\tCountry: ", "country"])
-                ip = ip.replace("country", "")
-            if "region" in ip:
-                shownFields.append(["\tRegion: ", "regionName"])
-                ip = ip.replace("region", "")
-            if "time zone" in ip:
-                shownFields.append(["\tTime Zone: ", "timezone"])
-                ip = ip.replace("time zone", "")
-                
-        else:
-            shownFields = [["\tCity: ", "city"], ["\tZIP Code: ", "zip"], ["\tISP: ", "isp"], ["\tCountry: ", "country"], ["\tRegion: ", "regionName"], ["\tTime Zone: ", "timezone"]]
-            ip = ip.replace("all", "")
-        
-        
-        ip = ip.strip()
-        ip = "".join([char for char in ip if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ":"]])
 
+        shownFields = []
+        if "all" not in query:
+            if "city" in query:
+                shownFields.append(["\tCity: ", "city"])
+                query = query.replace("city", "")
+            if "zip" in query:
+                shownFields.append(["\tZIP code: ", "zip"])
+                query = query.replace("zip", "")
+            if "isp" in query:
+                shownFields.append(["\tISP: ", "isp"])
+                query = query.replace("isp", "")
+            if "country" in query:
+                shownFields.append(["\tCountry: ", "country"])
+                query = query.replace("country", "")
+            if "region" in query:
+                shownFields.append(["\tRegion: ", "regionName"])
+                query = query.replace("region", "")
+            if "time zone" in query:
+                shownFields.append(["\tTime Zone: ", "timezone"])
+                query = query.replace("time zone", "")
+
+        else:
+            shownFields = [
+                ["\tCity: ", "city"],
+                ["\tZIP Code: ", "zip"],
+                ["\tISP: ", "isp"],
+                ["\tCountry: ", "country"],
+                ["\tRegion: ", "regionName"],
+                ["\tTime Zone: ", "timezone"]]
+            query = query.replace("all", "")
+
+        query = query.strip()
+        query = "".join([_ for _ in query if _ in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ":"]])
 
         try:
-            response = urlopen(URL + ip)
+            response = urlopen(GEOLOCATION_API + query)
             data = response.read()
             values = json.loads(data)
-        except:
-            print(f"Retrieving data for IP: {ip} failed, ensure you have a good internet connection")
+            print(values)
+        except BaseException:
+            print(
+                f"Retrieving data for IP: {ip} failed, ensure you have a good internet connection")
             continue
-        if values['status'] == "success":
-            
+        if values["status"] == "success":
+
             if shownFields:
-                print("Data dump for IP: " + values['query'])
+                print("Geodata for: " + values["query"])
                 for pair in shownFields:
                     print(pair[0] + values[pair[1]])
             else:
                 print("No valid options specified")
         else:
-            print(f"Retrieving data for IP: {ip} failed, ensure your IP is valid")
+            print(
+                f"Retrieving data for IP: {query} failed, ensure your IP is valid")
+
+
 if __name__ == "__main__":
-    print("\033[92m\033[1mStarting App...\033[94m")
+    print("[green bold]Starting App...[/green bold]")
     app()
