124598f9e5756e476b15d5d7cc51af7a29b69d95
deekb <derek.m.baier@gmail.com>
2022-07-28 13:14:03 -0400

    Fixed indentation

--- main.py
+++ main.py
@@ -3,57 +3,58 @@ import json
 from rich.console import Console
 from rich import print
 from urllib.request import urlopen
+
 GEOLOCATION_API = "http://ip-api.com/json/"
 
 
-def getIP():
-    return(urlopen("https://api.ipify.org/").read().decode())
+def get_host_ip():
+    return urlopen("https://api.ipify.org/").read().decode()
 
 
-def helpMenu():
-    return("Usage: [OPTIONS]... [IP Address(V4/V6)]\n" +
-           "Options:\n" +
-           "\tAll\t\tPrint all data about the address\n" +
-           "\tCity\t\tPrint the city of the address\n" +
-           "\tZIP\t\tPrint the ZIP code of the address\n" +
-           "\tISP\t\tPrint the Internet Service Provider(ISP) of the address\n" +
-           "\tCountry\t\tPrint the country of an address\n" +
-           "\tRegion\t\tPrint the region of an address\n" +
-           "\tTime Zone\tPrint the time zone of an address\n" +
-           "IP Address(V4/V6):\n")
+def help_menu():
+    return ("Usage: [OPTIONS]... [IP Address(V4/V6)]\n" +
+            "Options:\n" +
+            "\tAll\t\tPrint all data about the address\n" +
+            "\tCity\t\tPrint the city of the address\n" +
+            "\tZIP\t\tPrint the ZIP code of the address\n" +
+            "\tISP\t\tPrint the Internet Service Provider(ISP) of the address\n" +
+            "\tCountry\t\tPrint the country of an address\n" +
+            "\tRegion\t\tPrint the region of an address\n" +
+            "\tTime Zone\tPrint the time zone of an address\n" +
+            "IP Address(V4/V6):\n")
 
 
 def app():
     while True:
         query = input("Command (\"?\" for help): ").lower()
         if query == "?":
-            console = Console(highlight = False)
-            console.print(helpMenu())
+            console = Console(highlight=False)
+            console.print(help_menu())
             continue
 
-        shownFields = []
+        shown_fields = []
         if "all" not in query:
             if "city" in query:
-                shownFields.append(["\tCity: ", "city"])
+                shown_fields.append(["\tCity: ", "city"])
                 query = query.replace("city", "")
             if "zip" in query:
-                shownFields.append(["\tZIP code: ", "zip"])
+                shown_fields.append(["\tZIP code: ", "zip"])
                 query = query.replace("zip", "")
             if "isp" in query:
-                shownFields.append(["\tISP: ", "isp"])
+                shown_fields.append(["\tISP: ", "isp"])
                 query = query.replace("isp", "")
             if "country" in query:
-                shownFields.append(["\tCountry: ", "country"])
+                shown_fields.append(["\tCountry: ", "country"])
                 query = query.replace("country", "")
             if "region" in query:
-                shownFields.append(["\tRegion: ", "regionName"])
+                shown_fields.append(["\tRegion: ", "regionName"])
                 query = query.replace("region", "")
             if "time zone" in query:
-                shownFields.append(["\tTime Zone: ", "timezone"])
+                shown_fields.append(["\tTime Zone: ", "timezone"])
                 query = query.replace("time zone", "")
 
         else:
-            shownFields = [
+            shown_fields = [
                 ["\tCity: ", "city"],
                 ["\tZIP Code: ", "zip"],
                 ["\tISP: ", "isp"],
@@ -76,9 +77,9 @@ def app():
             continue
         if values["status"] == "success":
 
-            if shownFields:
+            if shown_fields:
                 print("Geodata for: " + values["query"])
-                for pair in shownFields:
+                for pair in shown_fields:
                     print(pair[0] + values[pair[1]])
             else:
                 print("No valid options specified")
